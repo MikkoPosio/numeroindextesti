72ebb39637c6fe388fc54b2e14e8c244f537b6ef
Mikko Posio <mikko.posio@oamk.fi>
2017-12-22 13:21:13 +0200

    Add files via upload

---
+++ Numeroindex testi.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 15 09:28:00 2017
+
+@author: mikkopos
+"""
+
+numerot = ["1", "2","3", "4","5", "6","7", "8"]
+Range = ["3", "2", "3", "5", "6", "12", "32", "43"]
+selectednumero = "3"
+numeroindex = numerot.index(selectednumero)
+termi = Range[numeroindex]
+print (" Valittu numero", termi)
+def x(y):
+    return 2 * y + 32
+auto = y(0.0)
